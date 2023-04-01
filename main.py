4dfa4fa34758b9fec33e47224d42350d045732ef
Rylan Davidson <GreenBeast2005@gmail.com>
2023-04-01 14:20:20 -0500

    The Final Project

--- main.py
+++ main.py
@@ -1,38 +1,91 @@
 import PySimpleGUI as pg
 
+total_CO2 = 0
+windowSize = 700
+
+
+def oven(days, total_CO2):
+    oven_CO2 = days * 52 * .28
+    total_CO2 = total_CO2 + oven_CO2
+    return total_CO2
+
+
+def microwave(days, total_CO2):
+    microwave_CO2 = days * 52 * .4
+    total_CO2 = total_CO2 + microwave_CO2
+    return total_CO2
+
+
+def washing_machine(days, total_CO2):
+    washing_machine_CO2 = days * 52 * .27
+    total_CO2 = total_CO2 + washing_machine_CO2
+    return total_CO2
+
+
+def electric_oven(days, total_CO2):
+    elec_oven_CO2 = days * 52 * .67
+    total_CO2 = total_CO2 + elec_oven_CO2
+    return total_CO2
+
+
+def dishwasher(days, total_CO2):
+    dishwasher_CO2 = days * 52 * .62
+    total_CO2 = total_CO2 + dishwasher_CO2
+    return total_CO2
+
+
+def flatscreen_tv(hours, total_CO2):
+    flatscreen_CO2 = hours * 365 * .098
+    total_CO2 = total_CO2 + flatscreen_CO2
+    return total_CO2
+
+
+def car(miles, total_CO2):
+    car_CO2 = miles * 52 * .404
+    total_CO2 = total_CO2 + car_CO2
+    return total_CO2
+
+
 # Step 1: Set theme
 pg.theme("DarkAmber")
 
 # Step 2: Create Layout
 layout = [
-    [pg.Button("Add Element")]
+    [pg.Text("How many times do you use your oven a week:", size=(46, 1)), pg.InputText(key='-oven-')],
+    [pg.Text("How many times do you use your microwave a week: ", size=(46, 1)), pg.InputText(key='-microwave-')],
+    [pg.Text("How many times do you use your washing machine a week: ", size=(46, 1)), pg.InputText(key='-washing-')],
+    [pg.Text("How many times do you use your electric oven a week: ", size=(46, 1)), pg.InputText(key='-electric-')],
+    [pg.Text("How many times do you use your dish washer a week: ", size=(46, 1)), pg.InputText(key='-dish-')],
+    [pg.Text("How many hours do you use your tv per day: ", size=(46, 1)), pg.InputText(key='-tv-')],
+    [pg.Text("How many miles do you drive per week: ", size=(46, 1)), pg.InputText(key='-car-')],
+    [pg.Button("Submit"), pg.Button("", key='value', visible=False)]
 ]
 
 # Step 3: Create Window
-window = pg.Window("Carbon Footprint Calculator", layout)
+window = pg.Window("Carbon Footprint Calculator", layout, size=(windowSize, int(windowSize/2)))
 
 # Step 4: Event loop
 while True:
     event, values = window.read()
     if event == pg.WIN_CLOSED:
         break
-    if event == "Add Element":
-        options = ["Oven", "Microwave", "TV"]
-        list_layout = [[pg.OptionMenu(values=options)],
-                       [pg.Button("Ok")]]
-        window = pg.Window('Options For whatever', list_layout)
-        event, values = window.read()
-    if  event == "Ok":
-        print(values[0])
+    if event == "Submit":
+        total_CO2 = 0
+        total_CO2 = oven(float(values['-oven-']), total_CO2)
+        total_CO2 = microwave(float(values['-microwave-']), total_CO2)
+        total_CO2 = washing_machine(float(values['-washing-']), total_CO2)
+        total_CO2 = electric_oven(float(values['-electric-']), total_CO2)
+        total_CO2 = dishwasher(float(values['-dish-']), total_CO2)
+        total_CO2 = flatscreen_tv(float(values['-tv-']), total_CO2)
+        total_CO2 = car(float(values['-car-']), total_CO2)
+        total_CO2 = round(total_CO2, 3)
+        window['value'].update(text=f'You produce about {total_CO2}kg of carbon per year', visible=True)
+    if event == 'value':
         layout = [
-            [pg.Button("1 hour")],
-            [pg.Button("2 hour")],
+            [pg.Text(f'In order to offset a carbon footprint of this size you would have to plant {int(round(total_CO2/20, 0))} trees')]
         ]
-        window = pg.Window("Times", layout)
-        event, values = window.read()
-
-    #print(values[0])
+        window.close()
+        window = pg.Window("Carbon Footprint Calculator", layout, size=(windowSize, int(windowSize/2)))
 
 # Step 5: Close window
 window.close()
-
